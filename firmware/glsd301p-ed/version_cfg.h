b0f335d7ce6a81f9ae2dc6a4ba769d2278396711
Jakub <jakub.holasek@live.com>
2026-09-07 23:38:36 +0200

    Add quarantined GL-SD-301P End Device build identity

---
+++ firmware/glsd301p-ed/version_cfg.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "comm_cfg.h"
+
+#define CHIP_TYPE                       TLSR_8258_512K
+#define APP_RELEASE                     0x7F
+#define APP_BUILD                       0x02
+#define STACK_RELEASE                   0x00
+#define STACK_BUILD                     0x01
+
+/* Quarantined development identity; not a stock GLEDOPTO firmware claim. */
+#define MANUFACTURER_CODE_TELINK        0x124F
+#define IMAGE_TYPE                      0x1416
+#define FILE_VERSION                    0x7F020001
+
+#define IS_BOOT_LOADER_IMAGE            0
+#define RESV_FOR_APP_RAM_CODE_SIZE      0
+#define IMAGE_OFFSET                    APP_IMAGE_ADDR
